30b1d9fbb2d8d29e2ab6e133ef2bfd3173c1c9ed
rynhdyt19 <104614030+rynhdyt19@users.noreply.github.com>
2024-01-02 23:07:20 +0800

    mengambil data dari json dan api

--- app/src/main/java/com/example/final_mobile/data/ApiInterface.kt
+++ app/src/main/java/com/example/final_mobile/data/ApiInterface.kt
@@ -1,8 +1,15 @@
 package com.example.final_mobile.data
 
+import com.example.final_mobile.data.models.CurrentWeather
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiInterface {
     @GET("weather?")
-    suspend fun getCurrentWeather()
+    suspend fun getCurrentWeather(
+        @Query("q") city : String,
+        @Query("units") units : String,
+        @Query("appid") apiKey : String,
+    ): Response<CurrentWeather>
 }
